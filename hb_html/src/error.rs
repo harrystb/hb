a1b3cb95588ef66d252bb36e4ca5947cbbfbc540
Harrison St Baker <harry@pop-os.localdomain>
2021-10-15 21:04:12 +1000

    Cleaned up folder stucture and remove memory arena
    Removed the memory arena in favour of a history recording graph pointer

--- hb_html/src/error.rs
+++ hb_html/src/error.rs
@@ -24,3 +24,30 @@ impl std::fmt::Debug for HtmlDocError {
         Ok(())
     }
 }
+
+pub struct ParseHtmlError {
+    msg: String,
+}
+
+impl ParseHtmlError {
+    pub fn new(msg: String) -> ParseHtmlError {
+        ParseHtmlError { msg: msg }
+    }
+
+    pub fn with_msg<S: Into<String>>(msg: S) -> ParseHtmlError {
+        return ParseHtmlError::new(msg.into());
+    }
+}
+
+impl std::fmt::Display for ParseHtmlError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "Html Doc Error: '{}'", self.msg)?;
+        Ok(())
+    }
+}
+impl std::fmt::Debug for ParseHtmlError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "Html Doc Error: '{}'", self.msg)?;
+        Ok(())
+    }
+}
--- hb_html/target/debug/libhb_html.d
+++
@@ -1,1 +0,0 @@
-/home/harry/dev/hb/hb_html/target/debug/libhb_html.rlib: /home/harry/dev/hb/hb_html/src/error.rs /home/harry/dev/hb/hb_html/src/lib.rs
