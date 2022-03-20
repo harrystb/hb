0b1db8adfa559dc78129104df9f1bb2483535f4e
Harrison St Baker <harry@pop-os.localdomain>
2022-03-20 15:10:53 +1000

    Some progress on parsing lib

--- hb_parse/src/lib.rs
+++ hb_parse/src/lib.rs
@@ -134,6 +134,40 @@ pub enum ParseEnding<'a> {
     BeforeStr(&'a str),
 }
 
+trait Parser {
+    fn parse(&mut self, c: char) -> Result<Option<(usize, usize)>, ParseError>;
+}
+
+struct CharParser {
+    start_index: usize,
+    chars_checked: usize,
+    ending: char,
+}
+
+impl CharParser {
+    fn new(ending: char, start_index: usize) -> CharParser {
+        CharParser {
+            start_index: start_index,
+            chars_checked: 0,
+            ending: ending,
+        }
+    }
+}
+
+impl<'a> Parser for CharParser {
+    fn parse(&mut self, c: char) -> Result<Option<(usize, usize)>, ParseError> {
+        if c == self.ending {
+            return Ok(Some((
+                self.start_index,
+                self.start_index + self.chars_checked,
+            )));
+        } else {
+            self.chars_checked += 1;
+            return Ok(None);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
