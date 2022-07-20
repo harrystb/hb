cf44062c0bc1003d459041be3cd76223e4fd4c72
Harrison St Baker <harry.stbaker@gmail.com>
2022-07-20 18:19:36 +1000

    testing some macros

--- hb_macros/examples/one.rs
+++ hb_macros/examples/one.rs
@@ -1,4 +1,4 @@
-use hb_macros::context;
+use hb_macros::{context, context2};
 use hb_parse::error::{ParseResult, ParseError};
 struct eh;
 
@@ -9,11 +9,17 @@ impl eh {
         Ok(())
     }
 
+    #[context2("Better?")]
+    pub fn hm() -> ParseResult<()> {
+        Err(ParseError::with_msg("inner"))
+    }
+
     pub fn b() -> ParseResult<()> {
         Err(ParseError::with_msg("inner"))
     }
 }
 
 fn main() {
-    println!("{}", eh::eh().err().unwrap())
+    println!("{}", eh::eh().err().unwrap());
+    println!("{}", eh::hm().err().unwrap())
 }
--- hb_macros/src/lib.rs
+++ hb_macros/src/lib.rs
@@ -2,7 +2,8 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::fold::{self, Fold};
 use syn::parse::{Parse, ParseStream, Result};
-use syn::{parse_macro_input, parse_quote, ItemFn, LitStr, Expr, ExprTry};
+use syn::{parse_macro_input, parse_quote, ItemFn, LitStr, Expr, ExprTry, Ident};
+use proc_macro2::Span;
 
 struct Msg {
     m: LitStr,
@@ -43,4 +44,21 @@ pub fn context(args: TokenStream, input: TokenStream) -> TokenStream {
     let output = message.fold_item_fn(input);
     // Hand the output tokens back to the compiler
     TokenStream::from(quote!(#output))
+}
+
+#[proc_macro_attribute]
+pub fn context2(args: TokenStream, input: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let mut input = parse_macro_input!(input as ItemFn);
+    input.attrs.retain(|x| x.path.segments.last().map_or_else(|| true, |a| a.ident != Ident::new("context2", Span::call_site())));
+    let block = input.block;
+    let message = parse_macro_input!(args as LitStr);
+    input.block = parse_quote!{
+        {
+            (||{#block})().map_err(|er| er.make_inner().msg(#message))
+        }
+    };
+
+    // Hand the output tokens back to the compiler
+    TokenStream::from(quote!{#input})
 }
